cb11761e620a5f7b661e2de88562d39f7dffad98
Manon Thiebaut <manonthiebaut62@gmail.com>
2023-03-06 11:22:18 +0100

    add app

---
+++ mindmaze-back/src/main/java/com/manon/mindmazeback/MindmazeBackApplication.java
@@ -0,0 +1,13 @@
+package com.manon.mindmazeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindmazeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindmazeBackApplication.class, args);
+	}
+
+}
---
+++ mindmaze-back/src/test/java/com/manon/mindmazeback/MindmazeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manon.mindmazeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindmazeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
